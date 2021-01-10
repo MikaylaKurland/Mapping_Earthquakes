0c29c1bd1c348a94622968d519e156776aee1ed1
MikaylaKurland <66794254+MikaylaKurland@users.noreply.github.com>
2021-01-10 09:06:45 -0500

    adding challenge code

--- Earthquake_Challenge/static/js/logicStep2.js
+++ Earthquake_Challenge/static/js/logicStep2.js
@@ -38,37 +38,17 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
 
     // This function returns the style data for each of the earthquakes we plot on
     // the map. We pass the magnitude of the earthquake into a function
-    // to calculate the color and radius.
+    // to calculate the radius.
     function styleInfo(feature) {
       return {
         opacity: 1,
         fillOpacity: 1,
-        fillColor: getColor(feature.properties.mag),
+        fillColor: "#ffae42",
         color: "#000000",
-        radius: getRadius(feature.properties.mag),
+        radius: getRadius(),
         stroke: true,
         weight: 0.5
       };
-    }
-
-    // This function determines the color of the circle based on the magnitude of the earthquake.
-    function getColor(magnitude) {
-      if (magnitude > 5) {
-        return "#ea2c2c";
-      }
-      if (magnitude > 4) {
-        return "#ea822c";
-      }
-      if (magnitude > 3) {
-        return "#ee9c00";
-      }
-      if (magnitude > 2) {
-        return "#eecc00";
-      }
-      if (magnitude > 1) {
-        return "#d4ee00";
-      }
-      return "#98ee00";
     }
 
     // This function determines the radius of the earthquake marker based on its magnitude.
@@ -91,14 +71,9 @@ d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
       },
 
       // We set the style for each circleMarker using our styleInfo function.
-      style: styleInfo,
+      style: styleInfo
 
-      // We create a popup for each circleMarker to display the magnitude and
-      //  location of the earthquake after the marker has been created and styled.
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-      }
-  }).addTo(map);
+    }).addTo(map);
 
 });
 
